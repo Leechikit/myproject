391c51ef8e19152103ef9cd48590460d41e0cfab
lizijie <leechikit0823@163.com>
2016-10-08 10:12:23 +0800

    delete old version

--- friday-system/server/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-
-require('./socket_server').listen(http);
-
-http.listen(8000, function(){
-  console.log('listening on *:8000');
-});
